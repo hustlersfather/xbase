742d3d7b172f33596c44236fd2ac812f911f3202
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-20 12:42:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -779,52 +779,28 @@ a.closearb {
 	
 	
 	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-
-<div class="main-home mt-5 mb-5">
-<div class="row">
-	
-<div class="col-md-8">
-
-<?php echo'
-<div class="card mb-3" style="color: var(--font-color); background-color: var(--color-card);">
-<div class="card-header"><i class="fas fa-user-shield"></i>
-Welcome <a class="badge badge-pill badge-success">'.$usrid.'</a><br>
+<?php
+ echo'
+<div class="form-group col-lg-7 ">
+<div class="well">
+  Hello <a class="label label-primary">'.$usrid.'</a><br>
+    If you have any <b>Question</b> ,<b>Problem</b>, <b>Suggestion</b> or <b>Request</b> Please feel free to <a class="label label-default " href="tickets.html"><span class="glyphicon glyphicon-pencil"></span> Open a Ticket</a><br>
+    if you want to report an order , just go to <abbr title="Account - > My Orders or Click here" >My Orders  <span class="glyphicon glyphicon-shopping-cart"></span></abbr> 
+    then click on <a class="label label-primary">Report #[Order Id]</a> button<br><br>
+    Our Domains are <b>Felux.shop</b>- Please Save them!
 </div>
-<div class="card-body" style="color: var(--font-color); background-color: var(--color-card);">
-<br>
-If you have any Question ,Problem, Suggestion or Request Please feel free to Open a
-<a href="tickets" class="btn btn-success btn-sm"> New Ticket <i class="fa fa-paper-plane"></i></a>
-<br>
-if you want to report an order , just go to <a class="btn btn-success btn-sm" data-title="My Orders" href="orders"> My Orders <i class="fa fa-shopping-cart"></i></a> then click on Report #[Order Id] button.
-<br>
-Our Domains are <p><span style="color: #ff0000;"><strong>odinshop.io</strong> || <strong>odin.pw</strong> || <strong>odinshop.se</strong> || <strong>odin.pm</strong> ||</span><strong><span style="color: #ff0000;">Tor Mirror odinshoyi3y5clejn3klyggxrmq5sy5efdiremz353z6ucdujgdiccad.onion ||</span> </strong></p> - Please Save them!
-<br>
-<a href="https://odinshop.se/Tos.php" data-title="Terms Of Service " class="btn btn-primary btn-sm"> Terms Of Service <i class="fa fa-newspaper-o"></i></a>
-<br>
-</div>
-</div>
+    <div class="list-group" id="div2">
+      	<h3><i class="glyphicon glyphicon-info-sign"></i> OUR NEWS</h3>'; 
+		 $qq = @mysqli_query($dbcon, "SELECT * FROM news ORDER by id desc LIMIT 5") or die("error here"); 
 
-<div class="card mb-3" style="color: var(--font-color); background-color: var(--color-card);">
-<div class="card-header">
-<i class="fas fa-newspaper"></i>Our News</div>'; 
-	$qq = @mysqli_query($dbcon, "SELECT * FROM news ORDER by id desc LIMIT 5") or die("error here"); 
-while($r = mysqli_fetch_assoc($qq)){				echo'<div class="card-body " style="color: var(--font-color); background-color: var(--color-card);">
-<ul><li class='mb-2'>'.stripcslashes($r['content']).'<span class='btn btn-info btn-sm mr-2 mt-1 mb-1'>
-		<b>News buyer</b>
-	</span>
-	  <span><small><b>'.$r['date'].'</small>
-		</span></b>'; ?>	
-	
+                
+while($r = mysqli_fetch_assoc($qq)){				echo'<a class="list-group-item">
+<h5 class="list-group-item-heading"><b>'.stripcslashes($r['content']).'</b></h5><h6 class="list-group-item-text">'.$r['date'].'</h6></a>'; 
+}
+ echo '
+			
+    ';
+	?>
 	
 	
 	
