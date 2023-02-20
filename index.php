6e0d0ddb3081e730858be44aee129fdad6e2c95d
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-20 06:05:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,10 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
   <title>XBASELEET</title>
   <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />  
-<!-- Bootstrap core CSS -->
+ <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="files/fonts/iconic/css/material-design-iconic-font.min.css">
+  <!-- Material Design Bootstrap -->
+	<!-- Bootstrap core CSS -->
 <link href="files/css/main.css" rel="stylesheet">
 	<!-- Bootstrap core CSS -->
 <link href="files/css/util.css" rel="stylesheet">
