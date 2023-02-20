d37a0bc974311ee71d25e76ddfff167496658b93
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-20 07:12:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -780,14 +780,18 @@ a.closearb {
 </div>
 </div>
 	
-	
+  <!-- Start your project here-->
+<?php eeho'
 <div class="main-home mt-5 mb-5">
 <div class="row">
 <div class="col-md-8">
 <div class="card mb-3" style="color: var(--font-color); background-color: var(--color-card);">
 <div class="card-header">
 <i class="fas fa-user-shield"></i>
-Welcome <a class="badge badge-pill badge-success">hustlersfather</a><br>
+Welcome <a class="badge badge-pill badge-success">'.$usrid'</a><br>
+
+  <!-- End your project here-->
+
 </div>
 
  </div>
