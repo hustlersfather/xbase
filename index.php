81f1f0b11ec2b363b2ee8b2126ed821e70b6e000
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-20 02:51:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
                                 $("#" + prop).html(data[prop]).show();
                             }
                         } else {
-                            window.location = "logout.html";
+                            window.location = "logout";
                         }
                     }
                 });
