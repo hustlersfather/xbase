3bdd25baae9c25b3f41f502d03941215de8f18f5
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-21 00:18:11 +0100

    Update leads.php

--- leads.php
+++ leads.php
@@ -10,6 +10,47 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
+
+
+<style>
+
+.display  td {
+    background: var(--color-card);
+      color: var(--font-color);
+
+}
+.dataTables_wrapper .dataTables_paginate .paginate_button {
+
+    color: var(--font-color);
+
+}
+#table_paginate .paginate_button {
+color: var(--font-color);
+
+}
+
+.alert-info {
+    color: var(--color-info);
+    background-color: var(--color-backinfo);
+    border-color: var(--color-borderinfo);
+
+}
+
+#table_filter{
+  color: var(--font-color);
+}
+#table_length{
+  color: var(--font-color);
+}
+#table_paginate{
+  color: var(--font-color);
+}
+#table_info{
+  color: var(--font-color);
+}
+</style>
+
+
 <style>
 .modal-dialog.modal-frame.modal-top.modal-notify.modal-danger .modal-body,.modal-dialog.modal-frame.modal-top.modal-offernov.modal-danger .modal-body{
 	    padding-top: 35px;
@@ -51,13 +92,14 @@ a.closearb {
 <li>There is <b> 0 </b> Available.</li>
 </ul>
 </div>
-<input type=hidden id="type" name="type" value="5" />
+<input type=hidden id="type" name="type" value="8" />
 <div class="row m-3 pt-1" style="color: var(--font-color);">
 <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
-<label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
+ <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
 <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
 </div>
 <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+	
 <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
 <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
 <option value="">All Countries</option>
@@ -129,8 +171,6 @@ a.closearb {
 
 </div>
 </div>
-
-
 <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
 <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
 
@@ -138,7 +178,7 @@ a.closearb {
 
 <div class="modal-body">
 <div class="row d-flex justify-content-center align-items-center">
-<img src="layout/images/balance.png">
+<img src="files/images/balance.png">
 <span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
 <a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
 <i class="fas fa-book ml-1 white-text"></i>
@@ -151,137 +191,6 @@ a.closearb {
 </div>
 </div>
 
-<script>
-
-        $(document).ready(function(){
-            var webID;
-            load_data();
-
-            function load_data(myarray) {
-                $('#lead_data').DataTable({
-                    "processing": true,
-                    "serverSide": true,
-                    "responsive": true,
-                    "scrollX": true,
-                    "order": [],
-                    "lengthMenu": [[10, 25, 50, 100, 500, 10000], [10, 25, 50, 100, 500, "All"]],
-                    "columnDefs": [
-                        {
-                            "targets": [ 0 ],
-                            "visible": false
-                        }
-                    ],
-
-                    "ajax":{
-                        url:"divPage6.html",
-                        type:"POST",
-                        data:{
-                            data_filter:myarray,
-                            type:document.getElementById('type').value,
-                             draw : 'draw',
-                             row : 'start',
-                             rowperpage : 'length',
-                             columnIndex : 'order',
-                             columnName : 'columns',
-                             columnSortOrder : 'order',
-                             searchValue : 'search'
-                              }
-                    },
-                 "columns": [
-                                { "data": 0 },
-                                { "data": 1 },
-                                { "data": 2 },
-                                { "data": 3 },
-                                { "data": 4 },
-                                { "data": 5 },
-                                { "data": 6 },
-                                { "data": 7 },
-                                { "data": 8 },
-                                { "data": 9 }
-                            ],
-
-                    "pageLength": 500
-                });
-            }
-
-            $(document).on('change', '.form-control', function(){
-                $('#lead_data').DataTable().destroy();
-                 var country = $('#country').val();
-                var description = $('#infos').val();
-                 var seller1 = $('#seller').val();
-                $idseller = seller1.split("Seller");
-                 var seller= $idseller[1];
-                 var myarray = {};
-
-                 myarray[0] = country;
-                 myarray[1] = description;
-                 myarray[2] = seller;
-
-              if(country != '' || description != '' || seller != '')
-                {
-
-                   load_data(myarray);
-                }
-                else
-                {
-                    load_data();
-                }
-                });
-
-
-        });
-
-       function buythistool(id){
-         $('#modalConfirmBuy').modal('show');
-         webID= id;
-                        }
-
-        function confirmbye(id){
-              id= webID;
-            $.ajax({
-                        method:"GET",
-                        url:"buytool.php?id="+id+"&t=leads",
-                        dataType:"text",
-                        success:function(data){
-                                if(data.match("buy")){
-                                let lastid = data.split("buy,")[1];
-                                $("#lead"+id).html(`<button onclick=openitem(${lastid}) class="btn btn-success btn-sm">Order ${'#'+lastid}</button>`).show();
-
-                            }
-                            else{
-                                if(data.match("deleted")){
-
-                                $("#lead"+id).html('Already sold / Deleted').show();
-
-                                  }else{
-                                   $('#modalCoupon').modal('show');
-                                }
-
-
-                            }
-                        },
-                    });
-        }
-    function openitem(order){
-
-
-        $.ajax({
-        type:       'GET',
-        url:        'showOrder'+order,
-        success:    function(data)
-        {
-        $("#myModalHeader").text('Order #'+order);
-        $("#modelbody").append(data);
-        $('#myModal').modal();
-
-
-        }});
-
-        }
-
-    </script>
-<script>(function(){var js = "window['__CF$cv$params']={r:'79c9e8b08871b8d8',m:'u12.r2kR.bSeN8qGHfh4KXngk_qGNCfyFFo9_HyFaUs-1676924070-0-AeR71Mez/8uAaLUB244+YrYlD134ASHeGDx9hT9j8Nk6VQLjiU9ht0OFU/IeJKFXJKHEgsTbUCFi4NhMmKv5w5CuaXghNBNQq57TOvwgfPnAbX0VBkPHQzbTOI3AS9wWJA==',s:[0xe876e4eeb4,0x8777c0601c],u:'/cdn-cgi/challenge-platform/h/g'};var now=Date.now()/1000,offset=14400,ts=''+(Math.floor(now)-Math.floor(now%offset)),_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/h/g/scripts/alpha/invisible.js?ts='+ts,document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.nonce = '';_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></body>
-</html>
 <?php
 include "includes/footer.php";  ?>
 </html>
