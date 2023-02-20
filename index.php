0f47841264cd228024e6f627bb470937f73ed047
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-20 16:26:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -704,7 +704,7 @@ a.closearb {
 <div class="row">
 <div class="col-md-10">
 <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
-<span class="h2 font-weight-bold mb-0">0 </div>
+<span class="h2 font-weight-bold mb-0"><?php echo'$r3';?></div>
 <div class="col-md-2">
 <div class="icon icon-shape bg-info text-white rounded-circle shadow">
 <i class="fa fa-shopping-cart"></i>
