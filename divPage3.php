38d360638e45a9b7c32742dd4bcec6aca51bdf60
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-20 13:23:32 -0800

    Update divPage3.php

--- divPage3.php
+++ divPage3.php
@@ -41,8 +41,11 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE
 </select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
 </div>
 
-<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
+<div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+<div class="col-sm-12 table-responsive">
+<table id=table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
 <thead>
+<tr><thead>
     <tr>
       <th scope="col" >Country</th>
       <th scope="col">TLD</th>
