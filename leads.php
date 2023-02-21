cee1ad171e244d8c002e7d282b88b4e3611a4156
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-21 01:20:34 +0100

    Update leads.php

--- leads.php
+++ leads.php
@@ -1,3 +1,16 @@
+<?php
+ob_start();
+session_start();
+date_default_timezone_set('UTC');
+include "includes/header.php";
+
+if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+    header("location: ../");
+    exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>
+
 <ul class="nav nav-tabs">
   <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
 </ul>
@@ -104,20 +117,3 @@ function openitem(order){
 }
 
 </script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
